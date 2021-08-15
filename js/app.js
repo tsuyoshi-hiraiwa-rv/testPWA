413b41c9353283c08d551fcac2f96ba5c3f9bc74
Tsuyoshi Hiraiwa <68318826+tsuyoshi-hiraiwa-rv@users.noreply.github.com>
2021-08-15 20:18:04 +0900

    参考にして作り直し

--- js/app.js
+++ js/app.js
@@ -1,35 +1,43 @@
-// 参考: https://knowledge.sakura.ad.jp/23201/
-// 参考: https://demi-urge.com/pwa-sw/
+// 変数定義
+let isPlaying = false
+let tapCount, time = 0
+const tapBtn    = document.getElementById('js-tapBtn')
+const startBtn  = document.getElementById('js-startBtn')
+const countText = document.getElementById('js-count')
+const timeText  = document.getElementById('js-time')
 
-//*****************************************************************************************
-//  インストール
-//*****************************************************************************************
-//バナーの代わりに表示するボタンを登録する
-registerInstallAppEvent(document.getElementById("InstallBtn"));
+// ゲームの初期値設定
+const setGame = () => {
+  tapCount = 0
+  time = 10000
+  countText.innerText = tapCount
+  timeText.innerHTML = time / 1000
+}
+setGame()
 
-//バナー表示をキャンセルし、代わりに表示するDOM要素を登録する関数
-//引数１：イベントを登録するHTMLElement
-function registerInstallAppEvent(elem){
-  console.log("インストールボタンのタップ");
+// タップした時にカウントを増やす
+tapBtn.addEventListener('click', () => {
+  if (!isPlaying) return false
+  tapCount++
+  countText.innerText = tapCount
+})
 
-  //インストールバナー表示条件満足時のイベントを乗っ取る
-  window.addEventListener('beforeinstallprompt', function(event){
-    console.log("beforeinstallprompt: ", event);
-    event.preventDefault(); //バナー表示をキャンセル
-    elem.promptEvent = event; //eventを保持しておく
-    elem.style.display = "block"; //要素を表示する
-    return false;
-  });
-  //インストールダイアログの表示処理
-  function installApp() {
-    if(elem.promptEvent){
-      elem.promptEvent.prompt(); //ダイアログ表示
-      elem.promptEvent.userChoice.then(function(choice){
-        elem.style.display = "none";
-        elem.promptEvent = null; //一度しか使えないため後始末
-      });//end then
+// STARTボタンを押してゲームをスタートさせる
+startBtn.addEventListener('click', () => {
+  setGame()
+  isPlaying = true
+  tapBtn.disabled = false
+  startBtn.style.display = 'none'
+
+  const timer = setInterval( () => {
+    time -= 10
+    timeText.innerHTML = (time / 1000).toFixed(2)
+
+    if (time === 0) {
+      clearInterval(timer)
+      isPlaying = false
+      startBtn.style.display = 'inline-block'
+      startBtn.innerText = 'もう一回'
     }
-  }//end installApp
-  //ダイアログ表示を行うイベントを追加
-  elem.addEventListener("click", installApp);
-}//end registerInstallAppEvent
+  }, 10)
+})
